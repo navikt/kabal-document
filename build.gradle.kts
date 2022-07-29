10cef114affb4f930bdd300c8d3e073ede7add05
Andreas Jonsson <andreas.jonsson@nav.no>
2022-07-29 13:18:57 +0200

    Updated kotlin plugin version

--- build.gradle.kts
+++ build.gradle.kts
@@ -41,9 +41,9 @@ repositories {
 }
 
 plugins {
-    kotlin("jvm") version "1.6.0"
-    kotlin("plugin.spring") version "1.6.0"
-    kotlin("plugin.jpa") version "1.6.0"
+    kotlin("jvm") version "1.7.10"
+    kotlin("plugin.spring") version "1.7.10"
+    kotlin("plugin.jpa") version "1.7.10"
     id("org.springframework.boot") version "2.5.12"
     id("io.spring.dependency-management") version "1.0.11.RELEASE"
     idea
