a3c1efa51816ae192e6187f5b969e29ec2d1263d
Øyvind N. Wedøe <oyvind.norsted.wedoe@nav.no>
2024-12-06 12:35:15 +0100

    Patch address for user missing address.
    Co-authored-by: Andreas Jonsson <andreas.jonsson@nav.no>

---
+++ src/main/resources/db/migration/V23__Patch_address.sql
@@ -0,0 +1,7 @@
+UPDATE document.avsender_mottaker
+SET adresse_adressetype    = 'norskPostadresse',
+    adresse_adresselinje_1 = 'Mørtelverksbakken 21, H0106',
+    adresse_postnummer     = '0580',
+    adresse_poststed       = 'OSLO',
+    adresse_land           = 'NO'
+WHERE id = '9845f8ab-9791-434a-a7e7-fd2501278310';
