83b7751218491b14e02e700ae60be923fd7e454f
Øyvind N. Wedøe <oyvind.norsted.wedoe@nav.no>
2025-11-19 11:04:50 +0100

    200 second timeout should be sufficient.

--- src/main/kotlin/no/nav/klage/dokument/config/WebClientConfig.kt
+++ src/main/kotlin/no/nav/klage/dokument/config/WebClientConfig.kt
@@ -16,7 +16,7 @@ class WebClientConfig {
 
     @Bean
     fun reactorNettyHttpClient(): HttpClient {
-        val timeoutInSeconds = 1000L
+        val timeoutInSeconds = 200L
         return HttpClient.create()
             .option(ChannelOption.CONNECT_TIMEOUT_MILLIS, 5_000)
             .responseTimeout(Duration.ofSeconds(timeoutInSeconds))
