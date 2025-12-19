89ac68aaa1fbe21d31491ba831b9997988af771b
Øyvind N. Wedøe <oyvind.norsted.wedoe@nav.no>
2025-12-19 11:18:35 +0100

    Adjusted postgres version in test.

--- src/test/kotlin/no/nav/klage/dokument/db/TestPostgresqlContainer.kt
+++ src/test/kotlin/no/nav/klage/dokument/db/TestPostgresqlContainer.kt
@@ -7,7 +7,7 @@ import org.testcontainers.containers.wait.strategy.HostPortWaitStrategy
 
 abstract class PostgresIntegrationTestBase {
     companion object {
-        private val postgres = PostgreSQLContainer("postgres:15.0").apply {
+        private val postgres = PostgreSQLContainer("postgres:15.4").apply {
             waitingFor(HostPortWaitStrategy())
             withReuse(true)
             start()
