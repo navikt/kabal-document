30c1674e7d7a8510b97601663d58f92256dca2f1
Andreas Jonsson <andreas.jonsson@nav.no>
2026-04-30 06:51:48 +0200

    Update Spring Boot.

--- build.gradle.kts
+++ build.gradle.kts
@@ -22,7 +22,7 @@ repositories {
 
 plugins {
     val kotlinVersion = "2.3.21"
-    id("org.springframework.boot") version "4.0.5"
+    id("org.springframework.boot") version "4.0.6"
     id("io.spring.dependency-management") version "1.1.7"
     kotlin("jvm") version kotlinVersion
     kotlin("plugin.spring") version kotlinVersion
